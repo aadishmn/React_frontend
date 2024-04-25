72e4f24b55d636965cb5d0eca8a76d3aec44125e
aadish <aadishmn@gmail.com>
2024-04-25 18:40:49 +0530

    react front_end

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+function App() {
+  return (
+    <>
+      <Router>
+          <Navbar/>
+        <Routes>
+          <Route path="/" exact Component={Home}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
